ff702d4f3fb66e59497c9ff35282f8d27dec06e8
mandoo1229 <skaalsdn35@gmail.com>
2024-08-30 13:56:49 +0900

    Flutter NFC기능

---
+++ lib/presentation/screen/read_write_nfc_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:minwoo/core/notifilter/nfc_notifiler.dart';
+import 'package:minwoo/presentation/widgets/dialogs.dart';
+import 'package:provider/provider.dart';
+class ReadWriteNFCScreen extends StatelessWidget {
+  const ReadWriteNFCScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => NFCNotifier(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("NFC READ/WRITE"),
+        ),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        scanningDialog(context);
+                        Provider.of<NFCNotifier>(context, listen: false)
+                            .startNFCOperation(nfcOperation: NFCOperation.read);
+                      },
+                      child: const Text("READ NFC")),
+                  ElevatedButton(
+                      onPressed: () {
+                        scanningDialog(context);
+                        Provider.of<NFCNotifier>(context, listen: false)
+                            .startNFCOperation(
+                            nfcOperation: NFCOperation.write,
+                            dataType: "URL");
+                      },
+                      child: const Text("WRITE NFC URL")),
+                  ElevatedButton(
+                      onPressed: () {
+                        scanningDialog(context);
+                        Provider.of<NFCNotifier>(context, listen: false)
+                            .startNFCOperation(
+                            nfcOperation: NFCOperation.write,
+                            dataType: "MAIL");
+                      },
+                      child: const Text("WRITE NFC EMAIL")),
+                  ElevatedButton(
+                      onPressed: () {
+                        scanningDialog(context);
+                        Provider.of<NFCNotifier>(context, listen: false)
+                            .startNFCOperation(
+                            nfcOperation: NFCOperation.write,
+                            dataType: "CONTACT");
+                      },
+                      child: const Text("WRITE NFC CONTACT")),
+                  Consumer<NFCNotifier>(builder: (context, provider, _) {
+                    if (provider.isProcessing) {
+                      return const CircularProgressIndicator();
+                    }
+                    if (provider.message.isNotEmpty) {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        Navigator.pop(context);
+                        showReulstDialog(context, provider.message);
+                      });
+                    }
+                    return const SizedBox();
+                  }),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
